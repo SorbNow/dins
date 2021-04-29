9cf412fc7335c8bac958df39e1f0120398f251b7
Murk <Murk@1.com>
2021-04-29 21:43:05 +0300

    add unit tests for services

--- src/test/java/com/sorb/dins/service/ContactServiceImplTest.java
+++ src/test/java/com/sorb/dins/service/ContactServiceImplTest.java
@@ -1,18 +1,22 @@
 package com.sorb.dins.service;
 
 import com.sorb.dins.model.Contact;
+import com.sorb.dins.model.Customer;
 import com.sorb.dins.repository.ContactRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 
 @SpringBootTest
 class ContactServiceImplTest {
@@ -34,46 +38,112 @@ class ContactServiceImplTest {
         contact = contactService.save(contact);
 
         assertNotNull(contact);
-        Mockito.verify(contactRepository,Mockito.times(1)).save(contact);
+        Mockito.verify(contactRepository, times(1)).save(contact);
     }
 
     @Test
     void getById() {
-        Contact returnedContact = new Contact();
-        returnedContact.setId(2);
-        returnedContact.setFirstName("Vasily");
-        returnedContact.setLastName("Red");
-        returnedContact.setPhoneNumber(779345);
+        Contact returnedContact = getSimpleContact();
 
         Mockito.when(contactRepository.findById(2L))
                 .thenReturn(Optional.of(returnedContact));
 
         Contact contact = contactService.getById(2);
         assertNotNull(contact);
-        assertEquals("Vasily",contact.getFirstName());
-        assertEquals(779345,contact.getPhoneNumber());
+        assertEquals("Vasily", contact.getFirstName());
+        assertEquals(779345, contact.getPhoneNumber());
 
-        Mockito.verify(contactRepository,Mockito.times(2)).findById(2L);
+        Mockito.verify(contactRepository, times(2)).findById(2L);
     }
 
     @Test
     void delete() {
+        final long contactId = 1L;
+        contactService.delete(contactId);
+        contactService.delete(contactId);
 
+        Mockito.verify(contactRepository, times(2)).deleteById(contactId);
     }
 
     @Test
     void getByCustomerId() {
+
+        Contact contact = getSimpleContact();
+        Mockito.when(contactRepository.findContactsByCustomer_Id(1L)).thenReturn(Collections.singletonList(contact));
+        List<Contact> contactList = contactService.getByCustomerId(1L);
+        Mockito.verify(contactRepository, times(1)).findContactsByCustomer_Id(1L);
+        assertEquals(1, contactList.size());
+        assertEquals(contact.getLastName(), contactList.get(0).getLastName());
+
+
     }
 
     @Test
     void updateContact() {
+        Contact contact = getSimpleContact();
+        Contact newContact = new Contact();
+        newContact.setFirstName("Amy");
+        newContact.setLastName("Fauler");
+        newContact.setPhoneNumber(4478);
+        Mockito.doReturn(contact)
+                .when(contactRepository)
+                .save(contact);
+        Contact updatedContact = contactService.updateContact(contact, newContact);
+        assertEquals("Amy", updatedContact.getFirstName());
+        assertNotEquals(0, updatedContact.getId());
+        Mockito.verify(contactRepository, times(1)).save(contact);
     }
 
     @Test
     void findContactByPhoneNumber() {
+        Mockito.doReturn(Collections.singletonList(getSimpleContact()))
+                .when(contactRepository)
+                .findContactsByPhoneNumberAndCustomer_Id(779345, 1L);
+
+        List<Contact> contacts = contactService.findContactByPhoneNumber(779345, 1L);
+        assertEquals(1, contacts.size());
+        assertNotNull(contacts.get(0));
+
+        Mockito.verify(contactRepository, times(1))
+                .findContactsByPhoneNumberAndCustomer_Id(779345, 1L);
+
     }
 
     @Test
     void deleteRelatedContacts() {
+        Contact contact = getSimpleContact();
+        Contact secondContact = new Contact();
+        secondContact.setId(1L);
+        secondContact.setFirstName("Martin");
+        secondContact.setLastName("Brundle");
+        secondContact.setPhoneNumber(11111);
+        secondContact.setCustomer(contact.getCustomer());
+        List<Contact> contactList = Arrays.asList(contact, secondContact);
+
+        assertEquals(2, contactList.size());
+
+        Mockito.doReturn(contactList).when(contactRepository).findContactsByCustomer_Id(any());
+
+        contactService.deleteRelatedContacts(1L);
+
+        Mockito.verify(contactRepository, times(1)).findContactsByCustomer_Id(1L);
+        Mockito.verify(contactRepository, times(2)).deleteById(any());
+
+    }
+
+    private Contact getSimpleContact() {
+
+        Customer customer = new Customer();
+        customer.setFirstName("Vanda");
+        customer.setLastName("Smith");
+        customer.setId(1L);
+
+        Contact returnedContact = new Contact();
+        returnedContact.setId(2);
+        returnedContact.setFirstName("Vasily");
+        returnedContact.setLastName("Red");
+        returnedContact.setPhoneNumber(779345);
+        returnedContact.setCustomer(customer);
+        return returnedContact;
     }
 }
---
+++ src/test/java/com/sorb/dins/service/CustomerServiceImplTest.java
@@ -0,0 +1,145 @@
+package com.sorb.dins.service;
+
+import com.sorb.dins.model.Contact;
+import com.sorb.dins.model.Customer;
+import com.sorb.dins.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+
+@SpringBootTest
+class CustomerServiceImplTest {
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerServiceImpl customerService;
+
+    @Test
+    void getAll() {
+        Mockito.doReturn(Collections.singletonList(createSimpleCustomer()))
+                .when(customerRepository).findAll();
+
+        Customer customer = customerService.getAll().get(0);
+        assertEquals(1, customer.getId());
+        assertEquals(2, customer.getContacts().size());
+
+        Mockito.verify(customerRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getById() {
+        Mockito.doReturn(Optional.of(createSimpleCustomer()))
+                .when(customerRepository).findById(any());
+
+        Customer customer = customerService.getById(1);
+        assertNotNull(customer.getFirstName());
+        assertEquals(1, customer.getId());
+        assertEquals("Peterson", customer.getLastName());
+
+        Mockito.verify(customerRepository, times(2)).findById(anyLong());
+    }
+
+    @Test
+    void delete() {
+
+        customerService.delete(1);
+        Mockito.verify(customerRepository, times(1)).deleteById(anyLong());
+    }
+
+    @Test
+    void save() {
+        Mockito.doReturn(createSimpleCustomer())
+                .when(customerRepository).save(any(Customer.class));
+
+
+    }
+
+    @Test
+    void updateCustomer() {
+
+        Customer customer = createSimpleCustomer();
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstName("Luke");
+        newCustomer.setLastName("Skywalker");
+        Mockito.doReturn(customer)
+                .when(customerRepository).save(customer);
+
+        Customer updatedCustomer = customerService.updateCustomer(customer, newCustomer);
+        Mockito.verify(customerRepository, times(1)).save(any(Customer.class));
+
+        assertEquals("Luke", customer.getFirstName());
+        assertEquals("Skywalker", customer.getLastName());
+        assertNotEquals(0, customer.getId());
+        assertNotNull(customer.getContacts());
+    }
+
+    @Test
+    void findCustomerByName() {
+        Mockito.doReturn(Collections.singletonList(createSimpleCustomer()))
+                .when(customerRepository).findCustomersByLastNameIgnoreCaseOrFirstNameIgnoreCase("Carl", "Carl");
+        Mockito.doReturn(Collections.singletonList(createSimpleCustomer()))
+                .when(customerRepository).findCustomersByLastNameIgnoreCaseContainsOrFirstNameIgnoreCaseContains("arl", "arl");
+
+        List<Customer> customers = customerService.findCustomerByName("Carl", false);
+
+        assertEquals(1, customers.size());
+
+        Customer customer = customers.get(0);
+
+        assertEquals("Carl", customer.getFirstName());
+        assertNotNull(customer.getContacts());
+
+        Mockito.verify(customerRepository, times(1))
+                .findCustomersByLastNameIgnoreCaseOrFirstNameIgnoreCase(anyString(), anyString());
+
+        customers = customerService.findCustomerByName("arl", true);
+
+        Mockito.verify(customerRepository, times(1))
+                .findCustomersByLastNameIgnoreCaseContainsOrFirstNameIgnoreCaseContains(anyString(), anyString());
+        assertEquals(1, customers.size());
+
+        customer = customers.get(0);
+
+        assertEquals("Carl", customer.getFirstName());
+        assertNotNull(customer.getContacts());
+    }
+
+    private Customer createSimpleCustomer() {
+        Customer simpleCustomer = new Customer();
+        simpleCustomer.setId(1L);
+        simpleCustomer.setFirstName("Carl");
+        simpleCustomer.setLastName("Peterson");
+
+        Contact contact = new Contact();
+        contact.setFirstName("Asoka");
+        contact.setLastName("Tano");
+        contact.setPhoneNumber(2222);
+        contact.setCustomer(simpleCustomer);
+
+        Contact contact2 = new Contact();
+        contact2.setFirstName("Obi");
+        contact2.setLastName("Kenobi");
+        contact2.setPhoneNumber(2222);
+        contact2.setCustomer(simpleCustomer);
+
+        Set<Contact> contacts = new HashSet<>();
+        contacts.add(contact);
+        contacts.add(contact2);
+
+        simpleCustomer.setContacts(contacts);
+
+        return simpleCustomer;
+
+    }
+
+}
