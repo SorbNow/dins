cc652de5a7964f45cb32678cd4663d6eabdbe44e
Murk <Murk@1.com>
2021-05-03 05:05:58 +0300

    Swagger customization

--- src/main/java/com/sorb/dins/controller/ContactController.java
+++ src/main/java/com/sorb/dins/controller/ContactController.java
@@ -4,7 +4,10 @@ import com.sorb.dins.exception.NotFoundInDatabaseException;
 import com.sorb.dins.model.Contact;
 import com.sorb.dins.service.ContactService;
 import com.sorb.dins.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,12 +37,21 @@ public class ContactController {
         this.customerService = customerService;
     }
 
+    @Operation(summary = "get contacts list by customer id. " +
+            "If phone number not empty - find contact with number in customer phone book")
     @GetMapping
     public List<Contact> getContactsByCustomerId(@Positive @RequestParam long customerId,
                                                  @RequestParam(required = false, defaultValue = "0") long phoneNumber) {
         return phoneNumber == 0 ? contactService.getByCustomerId(customerId) : contactService.findContactByPhoneNumber(phoneNumber, customerId);
     }
 
+    @Operation(summary = "save contact to customer phone book")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = {
+            @ExampleObject(
+                    name = "add contact JSON",
+                    value =
+                            "{ \"firstName\" : \"string\", \"lastName\": \"string\", \"phoneNumber\":1, \"customer\": { \"id\":1} }")
+    }))
     @PostMapping
     public Contact saveContact(@Valid @RequestBody Contact newContact) {
         if (customerService.getById(newContact.getCustomer().getId()) == null)
@@ -47,18 +59,31 @@ public class ContactController {
         return contactService.save(newContact);
     }
 
+    @Operation(summary = "get contact from database by id")
     @GetMapping("/{id}")
-    public Contact getContactById(@Positive @PathVariable long id) {
+    public Contact getContactById(@Parameter(description = "id of contact which should be searched")
+                                  @Positive @PathVariable long id) {
         return contactService.getById(id);
     }
 
+    @Operation(summary = "edit contact from database by id")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = {
+            @ExampleObject(
+                    name = "edit contact JSON",
+                    value =
+                            "{ \"firstName\" : \"string\", \"lastName\": \"string\", \"phoneNumber\":1 }")
+    }))
     @PutMapping("/{id}")
-    public Contact changeContact(@Positive @PathVariable long id, @RequestBody Contact changedContact) {
+    public Contact changeContact(@Parameter(description = "id of contact which should be edited")
+                                 @Positive @PathVariable long id,
+                                 @RequestBody Contact changedContact) {
         return contactService.updateContact(contactService.getById(id), changedContact);
     }
 
+    @Operation(summary = "delete contact from database by id")
     @DeleteMapping("/{id}")
-    public void deleteContact(@Positive @PathVariable long id) {
+    public void deleteContact(@Parameter(description = "id of contact which should be deleted")
+                              @Positive @PathVariable long id) {
         contactService.delete(id);
     }
 }
--- src/main/java/com/sorb/dins/controller/CustomerController.java
+++ src/main/java/com/sorb/dins/controller/CustomerController.java
@@ -2,11 +2,14 @@ package com.sorb.dins.controller;
 
 import com.sorb.dins.model.Customer;
 import com.sorb.dins.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
@@ -29,30 +32,50 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-
+    @Operation(summary = "get customer list by full/part of first and last name. If parameters empty-return all customers")
     @GetMapping
-    public List<Customer> getCustomers(@RequestParam(required = false) String name,
+    public List<Customer> getCustomers(@Parameter(description = "first or last name of sought customer")
+                                       @RequestParam(required = false) String name,
+                                       @Parameter(description = "if true - search by part of fist and last name")
                                        @RequestParam(defaultValue = "false", required = false) boolean isRequiresLike) {
         return name == null ? customerService.getAll() : customerService.findCustomerByName(name, isRequiresLike);
     }
 
+    @Operation(summary = "save customer to database")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = {
+            @ExampleObject(
+                    name = "add customer JSON",
+                    value =
+                            "{ \"firstName\": \"string\", \"lastName\": \"string\" }")
+    }))
     @PostMapping
     public Customer saveCustomer(@Valid @RequestBody Customer customer) {
         return customerService.save(customer);
     }
 
+    @Operation(summary = "get customer from database by id")
     @GetMapping("/{id}")
-    public Customer getCustomerById(@Positive @PathVariable long id) {
+    public Customer getCustomerById(@Parameter(description = "id of customer which should be searched") @Positive @PathVariable long id) {
         return customerService.getById(id);
     }
 
+    @Operation(summary = "edit customer in database")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(examples = {
+            @ExampleObject(
+                    name = "edit customer JSON",
+                    value =
+                            "{ \"firstName\": \"string\", \"lastName\": \"string\" }")
+    }))
     @PutMapping("/{id}")
-    public Customer changeCustomer(@Positive @PathVariable long id, @RequestBody Customer changedCustomer) {
+    public Customer changeCustomer(@Parameter(description = "id of customer which should be edited")
+                                   @Positive @PathVariable long id, @RequestBody Customer changedCustomer) {
         return customerService.updateCustomer(customerService.getById(id), changedCustomer);
     }
 
+    @Operation(summary = "delete customer from database")
     @DeleteMapping("/{id}")
-    public void deleteCustomer(@Positive @PathVariable long id) {
+    public void deleteCustomer(@Parameter(description = "id of customer which should be deleted")
+                               @Positive @PathVariable long id) {
         customerService.delete(id);
     }
 
